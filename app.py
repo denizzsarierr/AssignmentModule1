5e8820f37ada9964e23f0d6f7bfe648ac97fbaf6
Deniz Sarier <denizsarier41950@hotmail.com>
2026-04-28 16:51:02 +0200

    Feature Commit

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ def dot_product(X,Y):
 
     return np.dot(X,Y)
 
-
 def inverse(X):
 
     return np.linalg.inv(X)
@@ -18,5 +17,14 @@ def power(X,n):
 
     return np.linalg.matrix_power(X,n)
 
+def operations(X,Y,n):
+
+    print(f"Dot Product of X and Y: {dot_product(X,Y)}")
+    print("===========================================")
+    print(f"Inverse of X: {inverse(X)}")
+    print("===========================================")
+    print(f"Transpose of X: {transpose(X)}")
+    print("===========================================")
+    print(f"X to the power of {n}: {power(X,n)}")
 
 
